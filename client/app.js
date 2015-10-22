c1a719121c304f5a48938001cc6d36f4d50836ff
Liu Jiyang <2411mail@gmail.com>
2015-10-22 14:58:00 +0800

    search

--- client/app.js
+++ client/app.js
@@ -1,32 +1,46 @@
-PackageData = new ReactiveVar([]);
-IsLoading = new ReactiveVar(false);
+var options = {
+  keepHistory: 1000 * 60 * 5,
+  localSearch: true
+};
+var fields = ['packageName', 'description'];
+
+// creating a source with the name "packages" and  ask to search
+// 'packageName' and 'description' fields locally.
+PackageSearch = new SearchSource('packages', fields, options);
 
 Template.searchResult.helpers({
   getPackages: function() {
-    return PackageData.get();
+    // get data from the source - this return a reactive data source
+    return PackageSearch.getData({
+      // transform and highlight searched words
+      // all regExp matching has done for you
+      // you only have to do the word replacement only (to bold searching words)
+      transform: function(matchText, regExp) {
+        return matchText.replace(regExp, "<b>$&</b>")
+      },
+      sort: {_score: -1}
+    });
   },
 
   isLoading: function() {
-    return IsLoading.get();
+    console.log("loading is " + PackageSearch.getStatus().loading);
+
+    return PackageSearch.getStatus().loading;
+  },
+
+  metadata: function() {
+    return PackageSearch.getMetadata();
   }
 });
 
 Template.searchResult.rendered = function() {
-  getResults("");
+  PackageSearch.search('');
 };
 
 Template.searchBox.events({
   "keyup #search-box": _.throttle(function(e) {
     var text = $(e.target).val().trim();
-    getResults(text);
+    // sending search request to the server
+    PackageSearch.search(text);
   }, 200)
 });
-
-function getResults(text) {
-  IsLoading.set(true);
-  Meteor.call('search-packages', text, function(err, results) {
-    if(err) throw err;
-    PackageData.set(results);
-    IsLoading.set(false);
-  });
-}
--- server/app.js
+++ server/app.js
@@ -1,43 +1,52 @@
-Meteor.methods({
-  "search-packages": function(searchText) {
-    Meteor._sleepForMs(1000);
-    var words = searchText.trim().split(" ");
-    var lastWord = words[words.length -1];
-    var query = {
-      "bool": {
-        "must": [
-          {
-            "bool": {
-              "should": [
-                {"match": {"packageName": {"query": searchText}}},
-                {"prefix": {"packageName": lastWord}},
-                {"match": {"description": {"query": searchText}}},
-                {"prefix": {"description": lastWord}}
-              ]
-            }
+SearchSource.defineSource('packages', function(searchText, options) {
+  // simply do the query and return the result
+  Meteor._sleepForMs(1000);
+  var words = searchText.trim().split(" ");
+  var lastWord = words[words.length -1];
+  var query = {
+    "bool": {
+      "must": [
+        {
+          "bool": {
+            "should": [
+              {"match": {"packageName": {"query": searchText}}},
+              {"prefix": {"packageName": lastWord}},
+              {"match": {"description": {"query": searchText}}},
+              {"prefix": {"description": lastWord}}
+            ]
           }
-        ],
-        "should": [
-          {"match_phrase_prefix": {"packageName": {"query": searchText, slop: 5}}},
-          {"match_phrase_prefix": {"description": {"query": searchText, slop: 5}}},
-        ]
-      }
-    };
+        }
+      ],
+      "should": [
+        {"match_phrase_prefix": {"packageName": {"query": searchText, slop: 5}}},
+        {"match_phrase_prefix": {"description": {"query": searchText, slop: 5}}},
+      ]
+    }
+  };
 
-    var result =  EsClient.search({
-      index: "meteor",
-      type: "packages",
-      body: {
-        query: query
-      }
-    });
+  var result =  EsClient.search({
+    index: "meteor",
+    type: "packages",
+    body: {
+      query: query
+    }
+  });
 
-    var data = result.hits.hits.map(function(doc) {
-      var source = _.clone(doc._source);
-      source._score = doc._score;
-      return source;
-    });
+  var data = result.hits.hits.map(function(doc) {
+    var source = _.clone(doc._source);
+    source._score = doc._score;
+    return source;
+  });
 
-    return data;
-  }
+  // getting the metadata
+  var metadata = {
+    total: result.hits.total,
+    took: result.took
+  };
+
+  // return both data and metadata
+  return {
+    data: data,
+    metadata: metadata
+  };
 });
